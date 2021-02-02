63f69d9d74003cde97d40e0618c568bf777ac4a9
kammielatay <kameshialatay@gmail.com>
2021-02-02 09:40:12 -0700

    adding express template

---
+++ Develop/server.js
@@ -0,0 +1,23 @@
+// DEPENDENCIES
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+var app = express();
+
+// Sets an initial port. 
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Routes
+
+// Basic route that sends the user first to the AJAX Page
+
+// Starts the server to begin listening
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
