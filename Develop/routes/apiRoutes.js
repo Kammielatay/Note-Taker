8d47ba617352924136d4c42a8dbc3f08978018ab
kammielatay <kameshialatay@gmail.com>
2021-02-05 17:05:09 -0700

    creating getNotes(), updating app.get & app.post

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,20 +1,34 @@
 const fs = require("fs");
-const notes = require("../db/db.json");
+const { get } = require("http");
+//const notes = require("../db/db.json");
+var uniqid = require("uniqid");
 
+function getNotes() {
+  let notes = fs.readFileSync("./db/db.json", 'utf8') 
+  return JSON.parse(notes);
+}
 
 module.exports = (app) => {
 
   app.get("/api/notes", function (req, res) {
-    res.json(notes);
-  });
+    // This is using the getNotes function to read the db.json file 
+    let allNotes= getNotes()
+    res.json(allNotes)
+  })
 
   app.post("/api/notes", function (req, res) {
-    let newNote = req.body
-    notes.push(newNote);
-   
-    fs.writeFile("./db/db.json", JSON.stringify(notes), "utf8", function (err) {
+    // getting the new note and giving it a unique id
+    let newNote = req.body;
+    newNote.id = uniqid();
+
+    // This is reading all the notes from the db.json file and pushing our new note every time
+    let iNeedMyNotes = getNotes();
+    iNeedMyNotes.push(newNote);
+
+   // This is writing the file and sending back the request
+    fs.writeFile("./db/db.json", JSON.stringify(iNeedMyNotes), "utf8", function (err) {
       if (err) throw err;
-      res.json(req.body);
+      res.send(req.body);
   });
 })
 
